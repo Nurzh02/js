2de051c9b5c69735efa3dbdd39c014d1532fe368
Nurzhan <Nurzhan-.-93@mail.ru>
2022-07-08 16:33:25 +0600

    creat html file

---
+++ index.js
@@ -0,0 +1,51 @@
+// var obj = {a: 1, b: 2, c: 3};
+
+// num1 = 'a'
+// num2 = 'b'
+// num3 = 'c'
+
+// alert(['c']); ИЛИ alert(obj['c']);
+// objnum = ['c'];
+// console.log(obj);
+
+let array = [
+  {
+    name: "John",
+    age: 30
+  },
+  {
+    name: "Bob",
+    age: 21
+  },
+  {
+    name: "Anna",
+    age: 19
+  }
+  ]
+
+  for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
+  };
+
+// let array = [
+//   {
+//     name: "John",
+//     age: 30
+//   },
+//   {
+//     name: "Bob",
+//     age: 21
+//   },
+//   {
+//     name: "Anna",
+//     age: 19
+//   }
+
+//   ]
+
+//   objname = 'bob'
+//     for (let i = 0; i < array.length; i++) {
+//       console.log(array[i]) === 'name';
+//   }
+
+//   console.log(objname);
